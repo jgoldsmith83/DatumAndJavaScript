1e8c97210e0ef369e4f4d1d4e33bbc370f62249f
JGoldsmith <justingoldsmith@my.tridenttech.edu>
2015-08-28 17:46:25 -0400

    First commit

---
+++ Chapter 2/myFavMovie.js
@@ -0,0 +1,11 @@
+//Justin Goldsmith
+
+document.write("<h3>Good Will Hunting</h3>");
+
+document.write("<div>Good Will Hunting is a movie that follows the experiences of " +
+				"Will Hunting, a genius from the wrong side of town. During his " +
+				"employment at MIT, the staff discovers his genius and set out to " +
+				"put him to work for the benefit of the field when he is caught " +
+				"solving problems of extreme difficulty with ease. However, Will is " +
+				"incarcerated at the time of the discovery and as a part of his " +
+				"release deal is that he must see a therapist until his 21st birthday.</div>");
